dbc08ecf8a2141d379e01697af2966fd0452b0be
Piyush Sambyal <piyush.sambyall@gmail.com>
2026-07-16 11:03:10 +0530

    Ensure immediate data writing with f.flush()
    Added f.flush() to ensure data is written to the file immediately.

--- save_data.py
+++ save_data.py
@@ -1,17 +1,12 @@
 import serial
 import csv
-
-ser = serial.Serial('COM3',115200)   # change COM port
-
+ser = serial.Serial('COM3',115200)   # (COM 303) change COM port
 with open('sensor_data.csv','w',newline='') as f:
     writer = csv.writer(f)
-
     while True:
         line = ser.readline().decode().strip()
         print(line)
-
         data = line.split(',')
-
         if len(data)==7:
             writer.writerow(data)
             f.flush()
